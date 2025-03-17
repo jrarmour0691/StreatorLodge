b6019213de7eac102f691303b85da9e94bdb4da9
Jeffrey Armour <35618932+jrarmour0691@users.noreply.github.com>
2025-03-16 21:55:13 -0500

    Add files via upload

---
+++ Contact-Us/index.js
@@ -0,0 +1,13 @@
+// Select the image and its parent link
+const img = document.getElementById('Facebook');
+const link = img.parentElement;
+
+// Add event listener for mouseover to change the image source
+img.addEventListener('mouseover', () => {
+	img.src = "../img/Facebook Black.png";
+});
+
+// Add event listener for mouseout to revert the image source
+img.addEventListener('mouseout', () => {
+    img.src = '../img/Facebook Blue.png'; // Revert to the original image
+});
