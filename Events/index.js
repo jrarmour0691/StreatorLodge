07c99b4afc2dfae254e85cf1abb2710187651438
Jeffrey Armour <35618932+jrarmour0691@users.noreply.github.com>
2025-04-06 00:18:47 -0500

    Add files via upload

--- Events/index.js
+++ Events/index.js
@@ -1,3 +1,22 @@
+// Select logo elements text
+    const homeText = document.querySelector('.home-text');
+
+    // Function to toggle writing depending on screen size
+    function toggleLogo() {
+        const screenWidth = window.innerWidth;
+
+        if (screenWidth < 768) { // Define your breakpoint (e.g., 768px for smaller screens)
+            homeText.classList.remove('d-none'); // Show "Home Page"
+        } else {
+            homeText.classList.add('d-none'); // Hide "Home Page"
+        }
+    }
+
+    // Add event listeners
+    window.addEventListener('resize', toggleLogo); // Run on screen resize
+    window.addEventListener('load', toggleLogo);   // Run when the page is loaded
+
+
 // Select the image and its parent link
 const img = document.getElementById('Facebook');
 const link = img.parentElement;
@@ -20,13 +39,14 @@ function updateIframeSrc() {
   // Change the src dynamically based on screen width
   if (screenWidth > 479) {
     // For desktops or larger devices
-    	iframe.src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fstreator.lodge&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId";
+    iframe.src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fstreator.lodge&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId";
 	iframe.style.width = '500px';
-    	iframe.style.height = '500px';
-  } else {
+    iframe.style.height = '500px';
+  } 
+  else {
     // For tablets or smaller devices
     iframe.src = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fstreator.lodge&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId";
-    iframe.style.width = '300px';
+	iframe.style.width = '300px';
     iframe.style.height = '500px';
   }
 }
@@ -36,3 +56,6 @@ window.onload = updateIframeSrc();
 
 // Update the iframe src dynamically when the window is resized
 window.addEventListener('resize', updateIframeSrc());
+
+
+	
