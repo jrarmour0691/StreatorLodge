9196129a2e0c7ee50c8bfa6c4bff6de60bbc4109
Jeff Armour <jrarmour0691@gmail.com>
2025-12-21 22:21:14 -0600

    Updated Officers Page with 5 officers and list of other appointed officers

--- Officers/index.js
+++ Officers/index.js
@@ -1,11 +1,3 @@
-//update text below image
-	const carousel = document.getElementById('officersCarousel');
-const officerName = document.getElementById('officerName');
 
-carousel.addEventListener('slid.bs.carousel', function () {
-    const activeItem = carousel.querySelector('.carousel-item.active'); // Detect the active slide
-    const newCaption = activeItem.getAttribute('data-caption'); // Retrieve caption from `data-caption`
-    officerName.textContent = newCaption; // Update text below the carousel
-});
 
 
